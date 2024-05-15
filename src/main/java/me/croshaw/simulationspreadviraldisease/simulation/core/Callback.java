bd249e3db4d70cb3ca4243a29ab44cd4ce657588
Croshaw <crosafixx200215@gmail.com>
2024-05-16 00:14:36 +0500

    [FEATURE] Add People Functionality

---
+++ src/main/java/me/croshaw/simulationspreadviraldisease/simulation/core/Callback.java
@@ -0,0 +1,5 @@
+package me.croshaw.simulationspreadviraldisease.simulation.core;
+
+public interface Callback {
+    void execute();
+}
---
+++ src/main/java/me/croshaw/simulationspreadviraldisease/simulation/core/PeopleState.java
@@ -0,0 +1,5 @@
+package me.croshaw.simulationspreadviraldisease.simulation.core;
+
+public enum PeopleState {
+    HEALTHY, SICK, VACCINATED, IMMUNE
+}
