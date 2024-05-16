fae7d79fae0fda7378029cc377ac8a30c9369b8d
Croshaw <crosafixx200215@gmail.com>
2024-05-16 23:17:20 +0500

    [FEATURE] Add WorldTime and Season

---
+++ src/main/java/me/croshaw/simulationspreadviraldisease/simulation/core/time/WorldTime.java
@@ -0,0 +1,34 @@
+package me.croshaw.simulationspreadviraldisease.simulation.core.time;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class WorldTime {
+    private LocalDateTime dateTime;
+    public WorldTime() {
+        this(LocalDateTime.of(0, 1, 1, 0,0,0));
+    }
+    public WorldTime(LocalDateTime startDate) {
+        dateTime = startDate;
+    }
+    public void addTime(long secondsStep) {
+        dateTime = dateTime.plusSeconds(secondsStep);
+    }
+    public Season getSeason() {
+        return Season.of(dateTime.getMonth());
+    }
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+    public LocalDate getDate() {
+        return dateTime.toLocalDate();
+    }
+    public LocalTime getTime() {
+        return dateTime.toLocalTime();
+    }
+    public Month getMonth() {
+        return dateTime.getMonth();
+    }
+}
